a45c93681de16e3246a80fec0350bbfc0efac871
frouny <frouny@lapeutop.home>
2020-04-16 22:31:34 +0200

    debugged after fresh install

--- config/install.sh
+++ config/install.sh
@@ -4,6 +4,10 @@ LN=/usr/bin/ln
 PWD=$(/usr/bin/pwd)
 
 ${LN} -s ${PWD}/vimrc ${HOME}/.vimrc
-${LN} -s ${PWD}/XDefaults ${HOME}/.Xdefaults
+${LN} -s ${PWD}/Xdefaults ${HOME}/.Xdefaults
 ${LN} -s ${PWD}/vim ${HOME}/.vim
-${LN} -s ${PWD}/i3 ${HOME}/.config/i3
+if [ -e ${HOME}/.config/i3/config ]
+then
+    mv ${HOME}/.config/i3/config ${HOME}/.config/i3/config.old
+fi
+${LN} -s ${PWD}/i3/config ${HOME}/.config/i3/config
