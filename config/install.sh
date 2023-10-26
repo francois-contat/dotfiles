6b05147dfc249a140ab1dd7b17cfb563e5421505
François Contat <francois.contat@gmail.com>
2023-10-26 11:40:03 +0200

    Added support to rewrite the CTRL+Shift+C/V bindings over urxvt

--- config/install.sh
+++ config/install.sh
@@ -5,6 +5,7 @@ PWD=$(/usr/bin/pwd)
 
 ${LN} -s ${PWD}/vimrc ${HOME}/.vimrc
 ${LN} -s ${PWD}/Xdefaults ${HOME}/.Xdefaults
+${LN} -s ${PWD}/Xresources ${HOME}/.Xresources
 ${LN} -s ${PWD}/vim ${HOME}/.vim
 if [ -e ${HOME}/.config/i3/config ]
 then
